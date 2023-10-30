e759df884a8bd2645d60dfdbac11dfb2a7f879b5
nilp amin <s4581164@student.uq.edu.au>
2023-10-30 18:53:17 +0100

    fixed error in maths. Also removed useless cmake code.

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -12,7 +12,11 @@ uint64_t factorial(uint64_t val) {
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    return factorial(val)/factorial(val - val2);
+    if (val >= val2)
+    {
+        return factorial(val)/factorial(val - val2);
+    }
+    return 0;
 }
 
 
