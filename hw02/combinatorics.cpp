dd30afd04ec8ec00a43e48e8736b706252eb276a
nilp amin <s4581164@student.uq.edu.au>
2023-10-30 13:58:39 +0100

    added cmake list file and finished implementing combinatorics functions

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,18 +2,20 @@
 
 
 uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+    uint64_t result{1};
+    for (uint64_t i = 1; i <= val; ++i)
+    {
+        result *= i;
+    }
+    return result;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    return factorial(val)/factorial(val - val2);
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    return permutation(val, val2)/factorial(val2);
 }
