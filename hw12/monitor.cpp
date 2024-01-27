20ee46fc126225c7e97451e2c7d0ddd1ed0d636c
nilp amin <s4581164@student.uq.edu.au>
2024-01-28 00:45:08 +0100

    fixed error

--- hw12/monitor.cpp
+++ hw12/monitor.cpp
@@ -11,8 +11,7 @@ FileMonitor::FileMonitor(const std::string &targetpath,
     , interval{interval},
     target_path_{targetpath}
 {
-    // TODO: Initialize a mapping for all files in the target path
-    for (auto const& dir_entry : fs::directory_iterator{target_path_})
+    for (auto const& dir_entry : fs::recursive_directory_iterator{target_path_})
     {
         mapping_[dir_entry.path()] = fs::last_write_time(dir_entry);
     }
@@ -20,17 +19,13 @@ FileMonitor::FileMonitor(const std::string &targetpath,
 
 // Monitor the targetpath for changes and pass information to the logger in case of differences
 void FileMonitor::start(std::chrono::seconds timeout) {
-    // TODO: Monitor the system for the given amount of time.
-    // Every given interval, check for modifications to the files and log it
-
     // get the current time
     auto start_time = std::chrono::steady_clock::now();
 
     while (std::chrono::steady_clock::now() - start_time < timeout)
     {
-        std::cout << "in loop" << std::endl;
         // check for additions of files in the directory
-        for (auto const& dir_entry : fs::directory_iterator(target_path_))
+        for (auto const& dir_entry : fs::recursive_directory_iterator(target_path_))
         {
             auto find_key_it = mapping_.find(dir_entry.path());
 
@@ -43,22 +38,23 @@ void FileMonitor::start(std::chrono::seconds timeout) {
         }
 
         // check for modifications or deletions of files in the directory
-        for (auto const& file_info : mapping_)
+        for (auto it = mapping_.begin(); it != mapping_.end();)
         {
-            if (not fs::exists(file_info.first))
+            if (not fs::exists(it->first))
             {
                 // file deleted
-                logger.log(file_info.first, status::removed);
-                mapping_.erase(file_info.first);
+                logger.log(it->first, status::removed);
+                it = mapping_.erase(it);
             } else
             {
-                auto new_write_time = fs::last_write_time(file_info.first);
-                if (new_write_time != file_info.second)
+                auto new_write_time = fs::last_write_time(it->first);
+                if (new_write_time != it->second)
                 {
                     // file has been modified
-                    mapping_[file_info.first] = new_write_time;
-                    logger.log(file_info.first, status::changed);
+                    mapping_[it->first] = new_write_time;
+                    logger.log(it->first, status::changed);
                 }
+                ++it;
             }
         }
 
--- hw12/monitor.h
+++ hw12/monitor.h
@@ -4,7 +4,6 @@
 #include <unordered_map>
 #include <string>
 #include <filesystem>
-#include <iostream>
 
 #include "logger.h"
 
